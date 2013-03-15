6bf9ac63d19c0569b68de08deeb6e46ba3318238
skipkent <kwood@rcn.com>
2013-03-15 11:41:15 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Danjam::Application.config.secret_token = 'aab00952dded90c553180e62561bb459354931091bf63d0d1b0140997feac88eb7caafadfee624201c35af6302ea0377b62bf1313fbb20736a6c382399e5d762'
