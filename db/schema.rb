56870552a6780f0575d34659e29cea037dbd7e11
KWood <kwood@rcn.com>
2013-03-19 23:03:10 -0400

    removed scaffoldZ bogus file

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130318191741) do
+ActiveRecord::Schema.define(:version => 20130318235003) do
 
   create_table "comments", :force => true do |t|
+    t.integer  "song_id"
     t.string   "username"
     t.text     "message"
-    t.integer  "song_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "comments", ["song_id"], :name => "index_comments_on_song_id"
 
   create_table "songs", :force => true do |t|
     t.string   "title"
