b61b02976d2e8cf91da42cfe525ffc12dade725f
skipkent <kwood@rcn.com>
2013-03-30 13:00:28 -0400

    fix typo in users index view

--- app/controllers/bios_controller.rb
+++
@@ -1,6 +0,0 @@
-class BiosController < ApplicationController
-  
-  def index
-    @users = User.all
-  end
-end
--- test/unit/helpers/bios_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class BiosHelperTest < ActionView::TestCase
-end
