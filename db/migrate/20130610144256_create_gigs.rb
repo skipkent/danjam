922f35819d281bcbc0e3d427ae1aff1ef07ef9ca
skipkent <kwood@rcn.com>
2013-11-16 19:25:18 -0500

    ducky momo

--- db/migrate/20130610144256_create_gigs.rb
+++ db/migrate/20130610144256_create_gigs.rb
@@ -1,8 +1,8 @@
-class CreateGigs < ActiveRecord::Migration
-  def change
-    create_table :gigs do |t|
+# class CreateGigs < ActiveRecord::Migration
+#   def change
+#     create_table :gigs do |t|
 
-      t.timestamps
-    end
-  end
-end
+#       t.timestamps
+#     end
+#   end
+# end
--- db/migrate/20130610144517_gigs.rb
+++ db/migrate/20130610144517_gigs.rb
@@ -1,14 +1,14 @@
 class Gigs < ActiveRecord::Migration
-  # def up
-  #   create_table :gigs do |t|
-  #     t.text  :date
-  #     t.text  :venue
-  #     t.text  :description
-  #     t.text  :directions
-  #   end
-  # end
+  def up
+    create_table :gigs do |t|
+      t.text  :date
+      t.text  :venue
+      t.text  :description
+      t.text  :directions
+    end
+  end
 
-  # def down
-  #   drop_table :gigs
-  # end
+  def down
+    drop_table :gigs
+  end
 end
